b1219a83d16e83f14066a86d9a1033d811be06ae
geekbrains <ashulyaev93@mail.ru>
2020-08-14 20:08:30 +0400

    For pull

--- src/GB_HW8/ThirdOption.java
+++ src/GB_HW8/ThirdOption.java
@@ -3,7 +3,7 @@ package GB_HW8;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ThirdOption {
+public class ThirdOption { //Реализация без переменных;
     public static void main(String[] args){
         int[] array = {5, 1, 5, 2, 2, 4, 1, 4, 5, 1, 5, 3, 2, 4, 4, 4, 5, 1, 3, 4, 2, 2, 1, 2, 4, 4, 4, 5, 4, 3, 5, 4, 4,
                 5, 5, 1, 4, 1, 5, 3, 1, 4, 5, 3, 3, 4, 2, 2, 4, 4, 5, 5, 1, 1, 1, 4, 5, 5, 4, 4, 2, 4, 3, 1, 3, 3, 1, 1,
